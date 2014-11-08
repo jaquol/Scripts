9f33ca981d70b36a8a0ed040fdc61527ef27dc50
jaquol <javier.quilez@mssm.edu>
2014-11-08 13:48:20 -0500

    update

--- make.windows.n.size.from.intervals.py
+++ make.windows.n.size.from.intervals.py
@@ -5,12 +5,15 @@
 # Goal: Merges genomic intervals (BED) format to make windows of a specified size
 
 
+import sys
+
+
 # Variable parameters
-window_size = 1e6	# e.g. for 1 Mbps
+input_bed, window_size, output_bed  = sys.argv[1:4]
+window_size = int(window_size)
 
 
-# Paths
-input_bed = 'test.bed'
+outfile = open(output_bed, 'w')
 
 
 # Variables to store coordinates
@@ -22,39 +25,47 @@ total_lengths = []
 
 # Read in file 
 with open(input_bed) as f:
-	
-	for line in f:
+        
+        for line in f:
 
-		# Get genomic coordinates
-		chrom, start, end  = line.strip().split('\t')
-		start = int(start)
-		end = int(end)
-		chunk_length = end - start
+                # Get genomic coordinates
+                chrom, start, end  = line.strip().split('\t')
+                start = int(start)
+                end = int(end)
+                chunk_length = end - start
 
-		# Initialize list: min() is applied to non-empty lists
-		if len(start_coords) == 0:
-			start_coords = start_coords + [start]
-		
-		# Initialize current_chrom:
-		if current_chrom == '':
-			current_chrom = chrom
+                # Initialize list: min() is applied to non-empty lists
+                if len(start_coords) == 0:
+                        start_coords = start_coords + [start]
+                
+                # Initialize current_chrom:
+                if current_chrom == '':
+                        current_chrom = chrom
+                        print chrom
 
-		# Calculate additional bps to reach the required window size of callable
-		offset = window_size - sum(total_lengths)
-		#print chrom, min(start_coords), end, offset
-		if offset < chunk_length:
-			extra_bit = start + offset
-			print chrom, '\t', min(start_coords), '\t', extra_bit, '\t', sum(total_lengths) + extra_bit - start
-			total_lengths = []
-			start_coords = []
-			continue
-		
-		# New chromosome
-		if current_chrom != chrom:
-			total_lengths = []
-			start_coords = []
+                # Calculate additional bps to reach the required window size of callable
+                offset = window_size - sum(total_lengths)
+                #print chrom, min(start_coords), end, offset
+                if offset < chunk_length:
+                        extra_bit = start + offset
+                        line_to_print = [str(i) for i in [chrom, min(start_coords), extra_bit, sum(total_lengths) + extra_bit - start]]
+                        outfile.write("\t".join(line_to_print) +  "\n")
+                        total_lengths = []
+                        start_coords = []
+                        print chrom, min(start_coords), extra_bit, sum(total_lengths) + extra_bit - start
+                        continue
+                
+                # New chromosome
+                if current_chrom != chrom:
+                        print chrom
+                        total_lengths = []
+                        start_coords = []
 
-		# Update values		 
-		total_lengths = total_lengths + [chunk_length]
-		start_coords = start_coords + [start]
-		current_chrom = chrom
+                # Update values          
+                total_lengths = total_lengths + [chunk_length]
+                start_coords = start_coords + [start]
+                current_chrom = chrom
+
+
+f.close()
+outfile.close()
